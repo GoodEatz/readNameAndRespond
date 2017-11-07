d595bcda09da5e2fac9cdbbac246a17bd90cda12
GoodEatz <kenmytruong.94@gmail.com>
2017-11-06 20:39:47 -0800

    readNameAndRespond

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/readNameAndRespond', function(req, res, next) {
+    //expecting data variable called name --retrieve value using body-parser
+    var value_name = req.body.name;  //retrieve the data associated with name
+    res.send("hello " + value_name);
+});
+
+module.exports = router;
